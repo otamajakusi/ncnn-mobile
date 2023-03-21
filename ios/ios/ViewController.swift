4ef6d14fa6282261805be1cb0a2742c667d8c7ae
hiroyuki.obinata <hiroyuki.obinata@gmail.com>
2023-03-21 16:43:50 +0900

    add camera code

--- ios/ios/ViewController.swift
+++ ios/ios/ViewController.swift
@@ -5,15 +5,66 @@
 //  Created by hiroyuki obinata on 2023/03/21.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+
+    let captureSession = AVCaptureSession()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        captureSession.beginConfiguration()
+
+        guard let captureDevice = AVCaptureDevice.default(for: .video),
+            let deviceInput = try? AVCaptureDeviceInput(device: captureDevice),
+            captureSession.canAddInput(deviceInput)
+        else { return }
+
+        captureSession.addInput(deviceInput)
+
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "sampleBufferQueue"))
+        videoOutput.videoSettings = [
+            kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA
+        ]
+
+        captureSession.addOutput(videoOutput)
+
+        captureSession.commitConfiguration()
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.captureSession.startRunning()
+        }
     }
-
-
 }
 
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(
+        _ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer,
+        from connection: AVCaptureConnection
+    ) {
+
+        guard let pixelBuffe = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        let ciImage = CIImage(cvPixelBuffer: pixelBuffe)
+        let context = CIContext()
+        guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else { return }
+        let image = UIImage(cgImage: cgImage)
+        DispatchQueue.main.async {
+            let screenWidth = self.view.bounds.width
+            let screenHeight = self.view.bounds.height
+            self.imageView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
+            self.imageView.image = image
+            self.imageView.contentMode = .scaleAspectFit
+        }
+    }
+
+    func captureOutput(
+        _ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer,
+        from connection: AVCaptureConnection
+    ) {
+    }
+}
